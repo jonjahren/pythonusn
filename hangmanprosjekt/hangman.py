a74bab2d10bdbbd4c35af728736dd15d97b1b359
Jon Jahren <jon.jahren@gmail.com>
2019-08-23 01:49:15 +0200

    Lagt til meldingene som Martin skrev til programmet.

--- hangmanprosjekt/hangman.py
+++ hangmanprosjekt/hangman.py
@@ -16,17 +16,29 @@
 
 import random as ra
 
-# Leser inn alle linjene fra tekstfil "ordbok"
+#################################################
+# Leser inn meldinger til spiller og ordene man # 
+# skal gjette.                                  #
+#################################################
 
 dictionary_list = open("ordbok", "r").readlines() 
+lose_messages = open("lost", "r").readlines()
+duplicate_messages = open("duplicates", "r").readlines()
+tries_messages = open("tries", "r").readlines()
 
 # Length for å finne hvor mange ord i ordboka
 
 word_count = len(dictionary_list) 
+lose_count_messages = len(lose_messages)
+dup_messages_length = len(duplicate_messages)
+tries_messages_length = len(tries_messages)
 
 # -1 for å unngå out of bounds, velger et tilfeldig ord fra ordboken
 
-select_random = ra.randint(0, word_count-1) 
+select_random = ra.randint(0, word_count-1)
+select_random_lose = ra.randint(0, lose_count_messages-1)
+select_random_dup_messages = ra.randint(0, dup_messages_length-1)
+select_random_tries = ra.randint(0, tries_messages_length-1) 
 
 # strip() fjerner trailing whitespace, og dette bruker det tilfeldiget tallet for å 
 # velge et ord fra ordlisten
@@ -67,7 +79,7 @@ while True:
     input_letter = input() # Dette blir en string. 
     input_to_list = list(input_letter) # Konverterer string til liste
     
-    print(used_letters, input_to_list, ''.join(guess_word)) # Debug-print
+    #print(used_letters, input_to_list, ''.join(guess_word)) # Debug-print
     
     ######################################################
     #                                                    #
@@ -77,10 +89,10 @@ while True:
     ######################################################    
     
     for index in range(len(random_word_to_list)):
-        print(random_word_to_list, index, input_letter)
-        if random_word_to_list[index] == input_letter:
+        #print(random_word_to_list, index, input_letter) <- Debug print
+        if random_word_to_list[index].casefold() == input_letter.casefold():
             index_number.append(index)
-            print(random_word_to_list[index], input_letter)
+            #print(random_word_to_list[index], input_letter) <- debug print
     
     #######################################################
     #                                                     #
@@ -92,9 +104,13 @@ while True:
     #######################################################
     
     if index_number:
-        print("Bokstaven finnes i ordet.")
+        select_random_tries = ra.randint(0, tries_messages_length-1) 
+        random_guess_message = tries_messages[select_random_tries].strip()
+        print(random_guess_message)
         if input_letter in used_letters:
-            print("Duplikatbokstav.")
+            select_random_dup_messages = ra.randint(0, dup_messages_length-1)
+            duplicate_message_try = duplicate_messages[select_random_dup_messages].strip()
+            print(duplicate_message_try)
         else:
             used_letters.append(input_letter)
             
@@ -117,15 +133,17 @@ while True:
     # sammenligner dem. Om de er like så avsluttes spillet#
     #######################################################
     
-    
-    if ''.join(random_word_to_list) == ''.join(guess_word):
+    if ''.join(random_word_to_list).casefold() == ''.join(guess_word).casefold():
         print("Du vant! Løsningsordet var:", ''.join(guess_word))
         break
     
     # Enkel if-setning for å avslutte spillet om det er for mange forsøk
     
     if guesses >= max_guesses:
-        print("Du har brukt for mange forsøk, mannen ble hengt.")
+        select_random_lose = ra.randint(0, lose_count_messages-1)
+        random_lose_message = lose_messages[select_random_lose].strip()
+        print(random_lose_message)
+        break
     
     guesses = guesses + 1
     used_letters.sort()
